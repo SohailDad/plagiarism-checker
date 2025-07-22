783cc790fbcbebbc3de65bee7375fe21d6833d27
SohailDAD <dsohail866@gmail.com>
2025-07-22 18:44:35 +0500

    install tesseract.js library

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const {getData, postData, putData, deleteData} = require("./controllers/crud")
 const connectedDB = require("./config/db")
+// Import the Tesseract.js library
+const Tesseract = require('tesseract.js');
+
 const app = express();
 const port = 3000
 
 app.use(express.json());
 connectedDB()
 
+const path = require('path');
+
+const imagePath = path.join(__dirname, '..', 'project_related_data', 'image.png');
+
+Tesseract.recognize(
+  imagePath,
+  'eng', // Language: 'eng' for English
+//   {
+//     logger: (m) => console.log(m), // Optional: shows progress in console
+//   }
+).then((imagText) => {
+  console.log("Extracted Text:\n", imagText.data.text);
+}).catch((error) => {
+  console.error("OCR Error:", error);
+})
+
 app.get("/api",getData)
 app.post("/api/insertdata",postData)
 app.put("/api/updatedata/:id",putData)
 app.delete("/api/deletedata/:id",deleteData)
-
 
 
 app.listen(port,()=>{
